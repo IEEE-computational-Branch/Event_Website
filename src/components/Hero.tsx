3997e9e07c7c3b260e292a65e0f3ea952cfd9bb4
Aditya Kulshrestha <adi.kul358@gmail.com>
2023-09-29 20:54:58 +0530

    Fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@
 
 function Hero() {
   return (
-    <div className="bg-transparent text-white/95">
+    <div className="bg-transparent text-white/95 bg-cover bg-top " style={{backgroundImage: "url('https://img.freepik.com/free-vector/digital-earth-technology-news-background_1017-36675.jpg')"}}>
       <div className="h-screen w-full max-w-7xl mx-auto flex flex-col justify-center px-4 xl:px-0">
         <h1 className="font-display text-6xl max-md:text-5xl tracking-wider">Hello There!</h1>
         <p className="font-light mt-8 w-full max-w-[420px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { BsList, BsXLg } from "react-icons/bs"
+import { BsList, BsX } from "react-icons/bs"
 
 function Navbar() {
   const [navOpen, setNavOpen] = useState(false)
@@ -7,7 +7,7 @@ function Navbar() {
   const closeNav = () => setNavOpen(false)
 
   return (
-    <div className="bg-black/50 backdrop-blur-lg text-white absolute w-full z-50 top-0">
+    <div className="border-b border-white/40 backdrop-blur-lg text-white fixed w-full z-50 top-0">
       <div className="h-[64px] w-full max-w-7xl mx-auto flex flex-row items-center justify-between px-4 xl:px-0">
         <span className="text-xl font-medium">IEEE CIS</span>
 
@@ -21,11 +21,11 @@ function Navbar() {
         </div>
         
         <div className="hidden max-md:flex relative h-full aspect-square cursor-pointer">
-          <BsList onClick={openNav} className={`${!navOpen ? "block" : "hidden"} absolute top-0 bottom-0 m-auto right-0`} size={36} />
-          <BsXLg onClick={closeNav} className={`${navOpen ? "block" : "hidden"} absolute top-0 bottom-0 m-auto right-0`} size={30} />
+          <BsList onClick={openNav} className={`${!navOpen ? "block" : "hidden"} absolute top-0 bottom-0 m-auto right-0`} size={32} />
+          <BsX onClick={closeNav} className={`${navOpen ? "block" : "hidden"} absolute top-0 bottom-0 m-auto right-0`} size={32} />
         </div>
       </div>
-      <div className={`${navOpen ? "max-md:flex" : "hidden"} flex-col gap-y-6 text-white/90 px-4 pb-8 pt-4`}>
+      <div className={`${navOpen ? "max-md:flex" : ""} hidden flex-col gap-y-6 text-white/90 px-4 pb-8 pt-4`}>
         <span className="hover:text-white cursor-pointer">Home</span>
         <span className="hover:text-white cursor-pointer">About Us</span>
         <span className="hover:text-white cursor-pointer">Event</span>
