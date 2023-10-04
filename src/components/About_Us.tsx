5c2505ebaa36e94db55f582c392a538c33a941f5
Stealthy-CodeWalker <webdev.vanguard@gmail.com>
2023-10-04 13:15:13 +0530

    build deploy 1

--- src/components/About_Us.tsx
+++ src/components/About_Us.tsx
@@ -6,7 +6,9 @@ function About_Us() {
         <div className="h-screen w-full max-w-7xl mx-auto flex flex-col px-4 xl:px-0" style={{alignItems:"center",justifyContent:"center"}}>
         <h1 className="font-display text-6xl max-md:text-5xl tracking-wider">About Us</h1>
         <div className='my-10 flex flex-col-reverse sm:flex-row 'style={{justifyContent:"space-between",alignItems:"center"}}>
-        <h3 className='font-light mt-8 w-full text-justify md:mt-.5 md:pl-10' style={{paddingRight:'10px'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro dolores repellat illum amet ea quo quisquam quae magni explicabo ratione recusandae ipsum beatae, earum quas possimus illo dolorum ipsam reprehenderit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi impedit non ratione consectetur molestiae esse magni expedita adipisci! Numquam repudiandae nisi id dolor iusto rem, commodi animi exercitationem et totam nemo vel modi nihil sit necessitatibus? Harum reprehenderit quam dolorum quae quis, recusandae sapiente est.</h3>
+        <h3 className='font-light mt-8 w-full text-justify md:mt-.5 md:pl-10' style={{paddingRight:'10px'}}>
+        The IEEE club at SRMIST is passionate about fostering students' innovation, curiosity, and technological brilliance. They would have a platform to investigate the dynamic fields of electrical and electronics engineering, computer science, and allied fields.The club wants to be the most innovative, team-oriented, and student-run space on campus, which will encourage the development of fresh concepts.Without the ongoing assistance and efforts of the professors, mentors, and students, the club would not have been able to launch.The founding of the IEEE club at SRMIST is evidence of the dedication and spirit of cooperation among our instructors and students.
+        </h3>
           <img src={About} alt="" className='pr-7 md:pl-7 md:pt-10' style={{width:'24vw',minWidth:"150px"}}/>      
         </div>
       </div>
